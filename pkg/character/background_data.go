a7fa1aede64eae3a11c76b3787c053e835d6ab0f
mdbdba <mdbntl@gmail.com>
2025-06-25 20:36:52 -0500

    background additions

--- pkg/character/background_data.go
+++ pkg/character/background_data.go
@@ -67,4 +67,69 @@ var Backgrounds = map[string]Background{
 			},
 		},
 	},
+	"artist": {
+		Name: "Artist",
+		Description: "You doggedly practiced artistic pursuits before taking up the adventuring life. " +
+			"Countless hours of practice, reflection, and expression altered the way you see the world, " +
+			"and demand for your artistic expression grew. Perhaps you began an acrobat honing your body, " +
+			"a shadow puppeteer hungry for monstrous material, a dour thespian in search of a patron to " +
+			"admire your dark performances, or a bubbly singer enraptured by the applause of strangers. " +
+			"No matter what form your expression has taken, you still thrive where an audience waits to " +
+			"be entertained, frightened, or inspired.",
+		SkillProficiencyOptions: map[string]ChoiceOptions{
+			"skills": {
+				NumberToSelect: 2,
+				Options:        []string{"Acrobatics", "Insight", "Performance", "Persuasion"},
+			},
+		},
+		AdditionalProficiencies: []string{"artist tools"},
+		AdditionalProficiencyOptions: map[string]ChoiceOptions{
+			"tools": {
+				NumberToSelect: 1,
+				Options:        static_data.GetToolsetNames(),
+			},
+			"languages": {
+				NumberToSelect: 1,
+				Options:        LanguageNames(),
+			},
+		},
+		Equipment: []static_data.EquipmentPackContent{
+			{Name: "mirror", Quantity: 1},
+			{Name: "ink", Quantity: 1},
+			{Name: "pen", Quantity: 1},
+			{Name: "clothes fine", Quantity: 1},
+			{Name: "pouch", Quantity: 1},
+		},
+		Money: Money{GoldPieces: 4},
+		TalentOptions: map[string]ChoiceOptions{
+			"background_related": {
+				NumberToSelect: 1,
+				Options:        []string{"quick", "scrutinous", "trade skills"},
+			},
+		},
+		Motivations: map[string]map[int]string{
+			"artistic": {
+				1:  "Painting",
+				2:  "Sculpture",
+				3:  "Poetry",
+				4:  "Storytelling",
+				5:  "Acting",
+				6:  "Dancing",
+				7:  "Juggling",
+				8:  "Puppetry",
+				9:  "Music",
+				10: "Gymnastics",
+			},
+			"adventuring": {
+				1: "Adventuring secures my fortune while my art secures my reputation.",
+				2: "Adventuring inspires me by allowing me to meet new people and experience new places.",
+				3: "The thrills and terror of adventuring make me far more comfortable in front of even hostile crowds.",
+				4: "Adventuring develops skills for me to use when entertaining a crowd.",
+				5: "Adventuring puts me out of reach of the patron I’ve neglected.",
+				6: "My mentor was an adventurer. If their travels made them a master, it may work for me as well!",
+				7: "Adventuring is how I will find someone who appreciates my art as much as it deserves.",
+				8: "Tales of heroism born from adventuring will make my fans hungry for more of my art.",
+			},
+		},
+	},
 }
